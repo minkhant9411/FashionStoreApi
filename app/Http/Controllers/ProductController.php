61b8881aac3527e53c11308de2c3400b4389d07c
echo <minkhant9411@gmail.com>
2023-11-14 22:52:59 +0630

    addPaginationToPorduct

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,38 +10,68 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Schema;
 
-class ProductController extends Controller {
+class ProductController extends Controller
+{
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
         $query = [];
         $products = Product::with('brand', 'category');
         if ($request->query()) {
+
+            // if ($request->has('author_name')) {
+            //     $posts->whereHas('author', function ($query) use ($request) {
+            //         $query->where('name', 'like', '%' . $request->input('author_name') . '%');
+            //     });
+            // }
+
+            // // Filter by book date
+            // if ($request->has('book_date')) {
+            //     $posts->whereDate('book_date', $request->input('book_date'));
+            // }
+
+            // // Retrieve the filtered posts
+            // $filteredPosts = $posts->get();
+
+
+
+
             foreach ($request->query() as $key => $value) {
+
                 if ($key == "search") {
+
                     $query[] = ["title", 'like', "%$value%"];
+
                 } else if ($key == "brand" || $key == "category") {
+
                     $products->whereHas($key, function ($q) use ($value) {
                         $q->where('title', 'like', "%$value%");
                     });
+
                 } else if (!Schema::hasColumn('products', $key) && ($key == "brand" || $key == "category")) {
+
                     continue;
+
                 } else {
+
                     $query[] = [$key, '=', $value];
+
                 }
             }
-            $products = $products->where($query)->get();
+            $products = $products->where($query)->paginate(5);
             return new ProductCollection($products);
         } else {
-            return new ProductCollection(Product::all());
+            return new ProductCollection(Product::paginate(5));
         }
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreProductRequest $request) {
+    public function store(StoreProductRequest $request)
+    {
         $validated = $request->validated();
         $product = Product::create($validated);
         return new ProductResource($product);
@@ -50,14 +80,16 @@ class ProductController extends Controller {
     /**
      * Display the specified resource.
      */
-    public function show(Product $product) {
+    public function show(Product $product)
+    {
         return new ProductResource($product);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductRequest $request, Product $product) {
+    public function update(UpdateProductRequest $request, Product $product)
+    {
         $validated = $request->validated();
         $product->update($validated);
         return new ProductResource($product);
@@ -66,7 +98,8 @@ class ProductController extends Controller {
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product) {
+    public function destroy(Product $product)
+    {
         $product->delete();
         return response(['message' => 'deleted succesfully'], 200);
     }
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -7,13 +7,15 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductResource extends JsonResource {
+class ProductResource extends JsonResource
+{
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array {
+    public function toArray(Request $request): array
+    {
         return [
             "id" => $this->id,
             'title' => $this->title,
@@ -21,7 +23,7 @@ class ProductResource extends JsonResource {
             'color' => $this->color,
             'brand' => Brand::find($this->brand_id)->title,
             'category' => Category::find($this->category_id)->title,
-            'date of arrival' => $this->arrival_time,
+            'arrival_time' => $this->arrival_time,
         ];
     }
 }
