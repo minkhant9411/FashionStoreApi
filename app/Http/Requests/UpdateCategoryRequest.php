76fc90e5f7c471ebc7afe41eaaaa2cba6d5f884f
echo <minkhant9411@gmail.com>
2023-11-14 15:09:45 +0630

    CategoryApiCreated

--- app/Http/Requests/UpdateCategoryRequest.php
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -19,7 +19,7 @@ class UpdateCategoryRequest extends FormRequest {
      */
     public function rules(): array {
         return [
-            'title' => 'sometimes|required'
+            'title' => 'sometimes|required',
         ];
     }
 }
